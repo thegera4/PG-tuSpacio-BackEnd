b2a14fe74fe7d818e98c9926689253dcf893dec7
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-08-25 09:58:01 -0500

    BACKEND->SERVER->ADD ROUTES AND CONTROLLER CATEGORIES

---
+++ index.js
@@ -0,0 +1,14 @@
+const server = require("./src/app.js");
+const { database } = require("./src/db.js");
+require("dotenv").config(); //para leer las variables de entorno
+
+const PORT = process.env.PORT || 3001;
+
+// Syncing all the models at once.
+//Con {force:true} ==>es un sincronizado forzado por lo que se reescribe la Bd al recargar la app
+database.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server Listening in http://localhost:${PORT}/`);
+    
+  });
+});
