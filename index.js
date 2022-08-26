dad0a3a277f8fa1efef967f3760c654c6a2449df
Edinson <edinsonmadrid@gmail.com>
2022-08-26 02:17:53 -0300

    add uploadCategoryDb en Index
    se agrega la funcion que carga todas las categorias en desde la Api a la DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./src/app.js');
 const { database } = require('./src/db.js');
+const { uploadCategoryDb } = require('./src/controllers/uploadProductsDb');
 
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 //Con {force:true} ==>es un sincronizado forzado por lo que se reescribe la Bd al recargar la app
 database.sync({ force: false }).then(() => {
+  uploadCategoryDb()    //  Realiza la carga de la categoria en la db
   server.listen(PORT, () => {
     console.log(`Server Listening in http://localhost:${PORT}/`);
   });
