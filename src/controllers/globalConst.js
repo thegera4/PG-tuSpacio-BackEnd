6e3ae913caf568e1a87a218514ad052bb38b9d0d
Edinson <edinsonmadrid@gmail.com>
2022-08-26 02:23:53 -0300

    add globalConst
    se crea el archivo para el manejo de urls como variables

---
+++ src/controllers/globalConst.js
@@ -0,0 +1,9 @@
+/**
+ * UTILIZADO PARA UTILIZAR LAS URLS COMO VARIABLES GENERICAS
+ */
+
+const URL_PRODUCTS = "https://makeup-api.herokuapp.com/api/v1/products.json"                
+
+module.exports = {
+    URL_PRODUCTS
+}
