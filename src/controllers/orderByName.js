29238c81193f3b89790921b554f6aa70058c8673
santiagompereyra <santiagompereyra@gmail.com>
2022-08-30 18:14:05 -0300

    ordenamiento por nombre

---
+++ src/controllers/orderByName.js
@@ -0,0 +1,41 @@
+const { Product } = require("../db");
+const axios = require("axios")
+const { URL_API } = require("./globalConst")
+
+
+/* ORDERING BY NAME  */
+const orderProductsName = async (req, res, next) => {
+    const { orderby } = req.query;
+
+    const resp = await axios.get(URL_API)
+    const dataApi = resp.data
+
+    if (orderby === 'z-to-a') {   
+        try {
+            dataApi.sort((a, b) => {
+                // const nameA = a.name.replace(".",""); //remove the dot from the name
+                // const nameB = b.name.replace(".",""); //remove the dot from the name
+                if ( nameA  > nameB) return -1;
+                if (nameA < nameB) return 1;
+                return 0
+            })
+            res.send(dataApi)            
+        } catch (error) {
+            next(error);
+        }              //                    
+    } else if (orderby === 'a-to-z') {        
+        try {
+            dataApi.sort((a, b) => {
+                if (a.name > b.name) return 1;
+                if (a.name < b.name) return -1;
+                return 0
+            })         
+            res.send(dataApi)
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+module.exports = {
+    orderProductsName
+};
