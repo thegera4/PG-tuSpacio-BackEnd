f381af9700d4b4aad6a6f9fca8107f84f8065a13
Edinson <edinsonmadrid@gmail.com>
2022-08-30 16:52:26 -0300

    Correcciones de search y categorie

--- src/controllers/oneCategorie.js
+++ src/controllers/oneCategorie.js
@@ -10,11 +10,7 @@ const getOneCategorie = async (req, res, next) => {
     const resp = await axios.get(URL_API)
     const dataApi = resp.data
 
-     /* FILTRADO DE PAGINAS CON IMAGENES QUE NO FUNCIONAN */
-     const e = dataApi.filter(e => !e.image_link.includes("purpicks") )
-     const e1 = e.filter(e => !e.image_link.includes("static-assets.glossier") )
-     const e2 = e1.filter(e => !e.image_link.includes("imancosmetics") )
-    try {
+   try {
         let resultCategory = dataApi.filter(e => e.category === category)
 
         res.send(resultCategory);
--- src/controllers/searchProducts.js
+++ src/controllers/searchProducts.js
@@ -10,19 +10,10 @@ const getSearchProducts = async (req, res, next) => {
     const resp = await axios.get(URL_API)
     const dataApi = resp.data
     
-    /* FILTRADO DE PAGINAS CON IMAGENES QUE NO FUNCIONAN */
-    const e = dataApi.filter(e => !e.image_link.includes("purpicks") )
-    const e1 = e.filter(e => !e.image_link.includes("static-assets.glossier") )
-    const e2 = e1.filter(e => !e.image_link.includes("imancosmetics") )
-
 
     if (name) {
         try {
-            let resultCategory = e2.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
-
-            // product_type ? resultCategory = resultCategory.filter(e => e.product_type.toLowerCase().includes(product_type.toLowerCase())) : null
-            // categorie ? resultCategory = resultCategory.filter(e => e.category.toLowerCase().includes(categorie.toLowerCase())) : null
-
+            let resultCategory = dataApi.filter(e => e.name = name)
             res.send(resultCategory);
         } catch (error) {
             next(error);
@@ -36,9 +27,9 @@ const getSearchProducts = async (req, res, next) => {
             next(error);
         }
     } else if (categorie) {
+
         try {
-            let resultCategory = dataApi.filter(e => e.category.includes(categorie))
-    
+            let resultCategory = dataApi.filter(e => e.category = categorie)
             res.send(resultCategory);
         } catch (error) {
             next(error);
