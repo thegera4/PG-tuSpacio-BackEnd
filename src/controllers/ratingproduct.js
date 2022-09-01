47124c105653710066f7f1b5c147d9adf2d9856a
Edinson <edinsonmadrid@gmail.com>
2022-09-01 20:41:23 -0300

    modificacion order by rating

--- src/controllers/ratingproduct.js
+++ src/controllers/ratingproduct.js
@@ -5,18 +5,34 @@ const { URL_API } = require("./globalConst")
 
 /* GET ONE CATEGORY FROM JSON */
 const getRatingProduct = async (req, res, next) => {
-    const {ratingMax, ratingMin}  = req.query;
+    const {ratingBy}  = req.query;
 
     const resp = await axios.get(URL_API)
     const dataApi = resp.data
 
-    try {
-            let resultCategory = dataApi.filter(e => e.rating>= ratingMin && e.rating <= ratingMax);    
-
-            res.send(resultCategory);
+    if (ratingBy === 'max-min') {   
+        try {
+            dataApi.sort((a, b) => {return b.rating - a.rating})    
+            res.send(dataApi)            
         } catch (error) {
-                next(error);
-            }
+            next(error);
+        }              //                    
+    } else if (ratingBy === 'min-max') {        
+        try {
+            dataApi.sort((a, b) => {return a.rating - b.rating})         
+            res.send(dataApi)
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    // try {
+    //         let resultCategory = dataApi.filter(e => e.rating>= ratingMin && e.rating <= ratingMax);    
+
+    //         res.send(resultCategory);
+    //     } catch (error) {
+    //             next(error);
+    //         }
 }
 module.exports = {
     getRatingProduct
