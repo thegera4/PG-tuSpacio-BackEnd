96eeb275cc1662ae4d12fa6b9c48a2135e6e8246
Edinson <edinsonmadrid@gmail.com>
2022-09-03 00:43:28 -0300

    format login controllers

--- api/src/controllers/authorization/login.js
+++ api/src/controllers/authorization/login.js
@@ -2,25 +2,25 @@
 const { User } = require("../db");
 
 /* GET DETAIL PRODUCT FROM JSON */
-const getLogin = async (req, res, next) => { 
-    
+const getLogin = async (req, res, next) => {
+
     // const {nickname, name, picture, email, email_verified, sid} = req.oidc.user;
     // app.get('/admin',  (req, res) => {
-        res.send(JSON.stringify(req.oidc.user));
-        // console.log(email_verified)
+    res.send(JSON.stringify(req.oidc.user));
+    // console.log(email_verified)
     //   });
     // res.send("estoy en getLogin")
     // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-//       app.get('/profile', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.oidc.user));
-// });
+    //       app.get('/profile', requiresAuth(), (req, res) => {
+    //   res.send(JSON.stringify(req.oidc.user));
+    // });
 }
 
 
 
-        
-        module.exports = {
-            getLogin
-        };
+
+module.exports = {
+    getLogin
+};
 
 
