63ff068273c4ed5960fba5557cad7e91c9ed7ac4
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-09-12 22:57:13 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 // Syncing all the models at once.
 //Con {force:true} ==>es un sincronizado forzado por lo que se reescribe la Bd al recargar la app
 
-database.sync({ force: true}).then(() => {
+database.sync({ force: false}).then(() => {
   // uploadCategoryDb()    //  Realiza la carga de la categoria en la db  
   server.listen(PORT, () => {
     console.log(`Server Listening in http://localhost:${PORT}/`);
